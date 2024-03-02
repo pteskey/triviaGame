8a7825aae20be9956690c6fab042f8d66373e77e
pteskey <patrick.teskey@gmail.com>
2024-03-02 14:43:58 -0700

    fully commented client javascript file

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-// Some default variables for game options
+//  Default variables for game options
 let difficulty = "easy";
 let questionAmount = 5;
 const optionDisplay = document.getElementById("displayoptions");
@@ -42,9 +42,9 @@ async function fetchQuestions() {
   const questions = await response.json();
   return questions;
 }
-
+// Initialize game
 startGame = async () => {
-  // Send data to the server
+  // Async function to send data to local JSON server
   async function sendDataToDatabase(data) {
     console.log("Sending data:", data); // Log the data before sending it
     const response = await fetch("http://localhost:3000/api/data", {
@@ -70,6 +70,7 @@ startGame = async () => {
   let qNumID = 0;
   let score = 0;
   const questions = await fetchQuestions();
+  // Group and shuffle answers
   const answers = questions[qNumID].incorrectAnswers.concat(
     questions[qNumID].correctAnswer
   );
@@ -97,21 +98,23 @@ startGame = async () => {
   <h1 class="triviaTitle">Trivia Challenge</h1>
       `;
   app.innerHTML = html;
-
   let answered = false;
+  // Function to check the answer
   checkAnswer = (selectedChoice) => {
+    // Fade out the trivia container
     const triviaContainer = document.getElementById("triviaContainer");
-
     triviaContainer.classList.remove("fade-in");
     triviaContainer.classList.add("fade-out");
+
     let correctAnswer = `${questions[qNumID].correctAnswer}`;
     if (answered) {
       return;
     }
-    // Check if the selected answer is correct
+    // Conditional to check if the selected answer is correct or not
     if (selectedChoice.innerText === correctAnswer) {
       selectedChoice.style.backgroundColor = "#00FF73";
       score++;
+      // Send data to local JSON server
       const correctWrite = {
         questions: [
           {
@@ -122,12 +125,13 @@ startGame = async () => {
         ],
       };
       sendDataToDatabase(correctWrite);
+      // Goto next question after 1.5 seconds
       setTimeout(() => {
         nextQuestion();
       }, 1500);
     } else {
       selectedChoice.style.backgroundColor = "#FF6565";
-
+      // Send data to local JSON server
       const incorrectWrite = {
         questions: [
           {
@@ -138,10 +142,11 @@ startGame = async () => {
         ],
       };
       sendDataToDatabase(incorrectWrite);
+      // Goto next question after 1.5 seconds
       setTimeout(() => {
         nextQuestion();
       }, 1500);
-      // Option to highlight correct answer if you chose wrong
+      // Highlight correct answer if you chose wrong
       let choices = document.getElementsByClassName("answer");
       for (let i = 0; i < choices.length; i++) {
         if (choices[i].innerText === correctAnswer) {
@@ -149,14 +154,14 @@ startGame = async () => {
         }
       }
     }
-    //Lockout anymore clicking
+    //Lockout ability to answer
     answered = true;
   };
-
+  // Function to move to the next question
   nextQuestion = () => {
     //Reset answer status
     answered = false;
-
+    // Score result screen when all questions are answered
     if (qNumID == questionAmount - 1) {
       app.innerHTML = `<h2 class="titleText">You got ${score} of ${questions.length} answers correct.</h2>
        <div class="final-buttons"> 
@@ -166,12 +171,15 @@ startGame = async () => {
       `;
       return;
     }
+    //Increment question number
     qNumID++;
     let html = "";
+    // Group and shuffle answers
     const answers = questions[qNumID].incorrectAnswers.concat(
       questions[qNumID].correctAnswer
     );
     shuffle(answers);
+    // Template for Trivia questions and answers
     html += `
     <img
     class="questionMarkIcon"
