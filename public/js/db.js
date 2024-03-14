3d3fb61c618e8bbb60d65d6ff61ed3f4af06b94e
pteskey <patrick.teskey@gmail.com>
2024-03-14 16:28:58 -0600

    db connection error bugfix

--- public/js/db.js
+++ public/js/db.js
@@ -15,9 +15,6 @@ const db = pgp({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  ssl: {
-    rejectUnauthorized: process.env.NODE_ENV !== "development",
-  },
 });
 
 module.exports = db;
