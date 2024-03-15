41f7bf65dfd690f1c110de3e3cc95fb4fc6b93e5
pteskey <patrick.teskey@gmail.com>
2024-03-14 23:39:25 -0600

    bug fixing db ssl

--- public/js/db.js
+++ public/js/db.js
@@ -9,6 +9,11 @@ const pgp = require("pg-promise")({
   },
 });
 
-const db = pgp(process.env.DATABASE_URL + "?ssl=true");
+const db = pgp({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = db;
